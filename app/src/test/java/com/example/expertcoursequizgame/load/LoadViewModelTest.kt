5e973783e08c137503e0153aee3d4ecc34bbacca
Artur <artur.fatihov23@mail.ru>
2026-01-25 15:05:09 +0500

    QG-22 LoadViewModelTest

---
+++ app/src/test/java/com/example/expertcoursequizgame/load/LoadViewModelTest.kt
@@ -0,0 +1,152 @@
+package com.example.expertcoursequizgame.load
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class LoadViewModelTest {
+
+    @Test
+    fun caseFragment() {
+        val repository = FakeLoadRepository()
+        repository.expectResult(LoadResult.Success)
+        val observable = FakeUiObservable()
+        val viewModel = LoadViewModel(
+            repository = repository,
+            observable = observable
+        )
+        val fragment = FakeFragment()
+
+        viewModel.load(isFirstRun = true) // onViewCreated first time
+        assertEquals(LoadUiState.Progress, observable.postUiStateCalledList.first())
+        assertEquals(1, observable.postUiStateCalledList.size)
+
+        assertEquals(1, repository.loadCalledCount) // ping repository to get data
+
+        viewModel.startUpdates(observer = fragment) // onResume
+        assertEquals(1, observable.registerCalledCount)
+
+        assertEquals(
+            LoadUiState.Progress,
+            fragment.statesList.first()
+        ) //give cached progress ui state to fragment
+        assertEquals(1, fragment.statesList.size)
+
+        repository.returnResult() // get data from server
+        assertEquals(LoadUiState.Success, observable.postUiStateCalledList[1])
+        assertEquals(2, observable.postUiStateCalledList.size)
+        assertEquals(LoadUiState.Success, fragment.statesList[1])
+        assertEquals(2, fragment.statesList.size)
+    }ff
+
+    @Test
+    fun recreateActivity() {
+        val repository = FakeLoadRepository()
+        repository.expectResult(LoadResult.Error(message = "no internet"))
+        val observable = FakeUiObservable()
+        val viewModel = LoadViewModel(
+            repository = repository,
+            observable = observable
+        )
+        val fragment = FakeFragment()
+
+        viewModel.load(isFirstRun = true) //onViewCreated
+        assertEquals(LoadUiState.Progress, observable.postUiStateCalledList.first())
+        assertEquals(1, observable.postUiStateCalledList.size)
+        assertEquals(1, repository.loadCalledCount)
+
+        viewModel.startUpdates(observer = fragment) // onResume
+        assertEquals(1, observable.registerCalledCount)
+
+        assertEquals(LoadUiState.Progress, fragment.statesList.first())
+        assertEquals(1, fragment.statesList.size)
+
+        viewModel.stopUpdates() //onPause and activity death (onStop, onDestroy)
+        assertEquals(1, observable.unregisterCalledCount)
+
+        repository.returnResult()
+        assertEquals(1, fragment.statesList.size)
+        assertEquals(
+            LoadUiState.Error(message = "no internet"),
+            observable.postUiStateCalledList[1]
+        )
+        assertEquals(2, observable.postUiStateCalledList.size)
+
+        val newInstanceFragment = FakeFragment() //new instance of fragment after activity recreate
+
+        viewModel.load(isFirstRun = false) //onViewCreated after activity recreate
+        assertEquals(1, repository.loadCalledCount)
+        assertEquals(2, observable.postUiStateCalledList.size)
+
+        viewModel.startUpdates(observer = newInstanceFragment) // onResume after recreate
+        assertEquals(2, observable.registerCalledCount)
+
+        assertEquals(
+            LoadUiState.Error(message = "no internet"),
+            newInstanceFragment.statesList.first()
+        )
+        assertEquals(1, newInstanceFragment.statesList.size)
+
+    }
+}
+
+private class FakeFragment : (LoadUiState) -> Unit {
+    val statesList = mutableListOf<LoadUiState>()
+    override fun invoke(p1: LoadUiState) {
+        statesList.add(p1)
+    }
+}
+
+private class FakeLoadRepository : LoadRepository {
+
+    private var loadResult: LoadResult? = null
+    private var loadResultCallback: (LoadResult) -> Unit = {}
+
+    fun expectResult(loadResult: LoadResult) {
+        this.loadResult = loadResult
+    }
+
+    var loadCalledCount = 0
+
+    override fun load(resultCallback: (LoadResult) -> Unit) {
+        loadCalledCount++
+        loadResultCallback = resultCallback
+    }
+
+    fun returnResult() {
+        loadResultCallback.invoke(loadResult)
+    }
+}
+
+private class FakeUiObservable : UiObservable {
+
+    private var uiStateCached: LoadUiState? = null
+    private var observerCached: ((LoadUiState) -> Unit)? = null
+
+    var registerCalledCount = 0
+    override fun register(observer: (LoadUiState) -> Unit) {
+        registerCalledCount++
+        observerCached = observer
+        if (uiStateCached != null) {
+            observerCached!!.invoke(uiStateCached)
+            uiStateCached = null
+        }
+    }
+
+    var unregisterCalledCount = 0
+    override fun unregister() {
+        unregisterCalledCount++
+        observerCached = null
+    }
+
+    val postUiStateCalledList = mutableListOf<LoadUiState>()
+    override fun postUiState(uiState: LoadUiState) {
+        postUiStateCalledList.add(uiState)
+        if (observerCached == null) {
+            uiStateCached = uiState
+        } else {
+            observerCached!!.invoke(uiStateCached)
+            uiStateCached = null
+        }
+    }
+}
+

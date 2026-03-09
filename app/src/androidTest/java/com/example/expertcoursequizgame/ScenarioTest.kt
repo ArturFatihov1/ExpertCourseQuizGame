b402d5429449dd70514abe624c010af7c278c055
Artur <artur.fatihov23@mail.ru>
2026-03-09 13:32:01 +0500

    test Git Pull request 1

--- app/src/androidTest/java/com/example/expertcoursequizgame/ScenarioTest.kt
+++ app/src/androidTest/java/com/example/expertcoursequizgame/ScenarioTest.kt
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class ScenarioTest {
-
+    val test = "no test"
     @get:Rule
     val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
 
